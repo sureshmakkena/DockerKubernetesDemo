8323e669124b7e01acc3cd35c941f63e86f4d691
MS2018 <makkenasbabu@gmail.com>
2023-09-04 23:45:19 +0530

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,106 @@
+﻿using DockerKubernetesDemo.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerKubernetesDemo.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly DatabaseContext _ctx;
+        public PersonController(DatabaseContext ctx)
+        {
+
+            _ctx = ctx;
+
+        }
+        public IActionResult Index()
+        {
+            ViewBag.greeting = "Hello Suresh";
+            ViewData["greetingGM"] = "Good Morning";
+            TempData["greetingTemp"] = "This is Temp data";
+            return View();
+        }
+
+        public IActionResult AddPerson()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                _ctx.Add(person);
+                _ctx.SaveChanges();
+                TempData["Status"] = "Person Added Sucessfully!!!";
+                return RedirectToAction("AddPerson");
+            }
+            catch (Exception ex)
+            {
+                TempData["Status"] = "Person could not be Added";
+                return View();
+            }
+
+        }
+
+        public IActionResult DisplayPersons()
+        {
+
+            var persons = _ctx.Person.ToList();
+
+            return View(persons);
+
+        }
+
+        public IActionResult EditPerson(int id)
+        {
+            var person = _ctx.Person.Find(id);
+
+            return View(person);
+        }
+        [HttpPost]
+        public ActionResult EditPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            try
+            {
+                _ctx.Update(person);
+                _ctx.SaveChanges();
+                TempData["Status"] = "Person updated Sucessfully!!!";
+                return RedirectToAction("DisplayPersons");
+            }
+            catch (Exception ex)
+            {
+                TempData["Status"] = "Person could not be Updated";
+                return View();
+            }
+        }
+
+        public IActionResult DeletePerson(int id)
+        {
+            try
+            {
+                var person = _ctx.Person.Find(id);
+
+                if (person != null)
+                {
+                    _ctx.Person.Remove(person);
+                    _ctx.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return RedirectToAction("DisplayPersons");
+        }
+    }
+}
---
+++ Models/Domain/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DockerKubernetesDemo.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+       
+        public string? Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
